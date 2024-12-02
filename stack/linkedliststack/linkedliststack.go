cfce33ed1aad90f6a913bf115e45f15fc033daae
Duc Thang Tran <ducthang.tran@covestro.com>
2024-12-02 18:20:11 +0700

    docs: adding comments for stack documentation

--- stack/linkedliststack/linkedliststack.go
+++ stack/linkedliststack/linkedliststack.go
@@ -2,18 +2,26 @@ package linkedliststack
 
 import "github.com/TranThang-2804/golangds/list/linkedlist"
 
+// This is the stack implementation using linked list
 type Stack[T comparable] struct {
 	list *linkedlist.LinkedList[T]
 }
 
+// Push the items into the stack
 func (s *Stack[T]) Push(values ...T) {
 	s.list.Prepend(values...)
 }
 
+// Get the value of the item at the top of
+// the stack, return the value of the item and
+// true if the item is found else return false
 func (s *Stack[T]) Peek() (T, bool) {
 	return s.list.Get(0)
 }
 
+// Pop the item from the top of the stack
+// return the value of the item and true if
+// the item is found else return false
 func (s *Stack[T]) Pop() (T, bool) {
 	value, err := s.Peek()
 	if err {
@@ -27,18 +35,22 @@ func (s *Stack[T]) Pop() (T, bool) {
 	return value, true
 }
 
+// Empty the stack
 func (s *Stack[T]) Empty() {
 	s.list.Clear()
 }
 
+// Get the size of the stack
 func (s *Stack[T]) Size() int {
 	return s.list.GetSize()
 }
 
+// Check if the stack is is empty
 func (s *Stack[T]) IsEmpty() bool {
-  return s.list.IsEmpty()
+	return s.list.IsEmpty()
 }
 
+// Return the string representation of the stack
 func (s *Stack[T]) String() string {
-  return s.list.String()
+	return s.list.String()
 }
