df27f2a1be1f61b200196545ec20667d8c9b71d5
Duc Thang Tran <ducthang.tran@covestro.com>
2024-12-14 16:45:10 +0900

    feat: adding queue for linkedlist implementation

--- queue/linkedlistqueue/queue.go
+++ queue/linkedlistqueue/queue.go
@@ -3,9 +3,35 @@ package linkedlistqueue
 import "github.com/TranThang-2804/golangds/list/linkedlist"
 
 type LinkedListQueue[T comparable] struct {
-  linkedList *linkedlist.LinkedList[T]
+	linkedList *linkedlist.LinkedList[T]
 }
 
 func New[T comparable]() *LinkedListQueue[T] {
-  return &LinkedListQueue[T]{}
+	return &LinkedListQueue[T]{}
+}
+
+func (q *LinkedListQueue[T]) Enqueue(value T) {
+	q.linkedList.Append(value)
+}
+
+func (q *LinkedListQueue[T]) Dequeue() (T, bool) {
+  // Get the first element of the linked list
+	t, err := q.linkedList.Get(0)
+
+	if !err {
+		return t, false
+	}
+
+  // Remove the first element of the linked list
+	err = q.linkedList.Remove(0)
+
+  if !err {
+    return t, false
+  }
+
+	return t, true
+}
+
+func (q *LinkedListQueue[T]) Peek() (T, bool) {
+  return q.linkedList.Get(0)
 }
